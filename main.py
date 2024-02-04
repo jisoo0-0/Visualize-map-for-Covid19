db1525b64a61fbcedab8e141a0b44726f954bc15
jisoo0-0 <ji_soo_o@korea.ac.kr>
2024-02-04 21:33:05 +0900

    modify the file

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from make_dataset import *
 def main():
     parser = argparse.ArgumentParser(description='Setting')
     parser.add_argument('--num_bins', type=int, default=50)
+    parser.add_argument('--cmap', type=str, default='PiYG_r')
+    parser.add_argument('--selected_col', type = str, default =r"Confirmed Cases")
     parser.add_argument('--csv_path', type = str, default =r"E:\VisualStudio_Code\Covid\covid_image\result_time_newconfirmed.csv")
     parser.add_argument('--map_path', type = str, default=r'E:\VisualStudio_Code\Covid\covid_image\ImagePreProcessing\rawdata\USCounties_JHUmap\USCounties_JHUmap.shp')
     parser.add_argument('--img_save_path', type=str, default=r'E:\VisualStudio_Code\Covid\covid_image\ImagePreProcessing\clean_data\newconfirmed\rgbcolor')
@@ -20,7 +22,7 @@ def main():
        'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah',
        'Vermont',  'Virginia', 'Washington',
        'West Virginia', 'Wisconsin', 'Wyoming']
-    save_imgs(args.csv_path, args.map_path, args.img_save_path, col_name, args.num_bins)
+    save_imgs(args.csv_path, args.map_path, args.img_save_path, col_name, args.num_bins, args.selected_col)
     
 if __name__ == '__main__':    
     main()
--- make_dataset.py
+++ make_dataset.py
@@ -7,30 +7,29 @@ import matplotlib.pyplot as plt
 import mapclassify
 from os.path import join as opj
 import numpy as np
-def save_imgs(csv_path, map_path, img_save_path, col_name, num_bins):
+def save_imgs(csv_path, map_path, img_save_path, col_name, num_bins, selected_col):
     
     df = pd.read_csv(csv_path, index_col=[0])
 
 
     covid_map = gpd.read_file(map_path)
-    covid_map = covid_map.drop('Confirmed',axis=1)
+    covid_map = covid_map.drop(f'{selected_col}',axis=1)
     covid_map = covid_map.dropna(subset=['geometry'])
     covid_map = covid_map.drop_duplicates()
-    col_name2 = set(df.columns) - set(col_name)
+    col_name2 = set(df.columns) - set(col_name) #Preprocessing part, it could be removed in your case
     df = df.drop(col_name2, axis=1)
-
-    scheme = mapclassify.Quantiles(df['Confirmed'], k=num_bins)
+    scheme = mapclassify.Quantiles(df[f'{selected_col}'], k=num_bins)
     scheme_list = scheme.bins
     covid_map = covid_map[covid_map['ST_Name'].isin(col_name)]
-
     uni_dt = df.index
     print(len(uni_dt),"*"*50)
-    for i in tqdm(len(uni_dt)): #날짜별로
+    
+    for i in tqdm(len(uni_dt)): #Save plots by date
         try:
             covid_plot = covid_map.merge(df.iloc[i], right_on=['Countyname','ST_Name'], left_on=['Countyname','ST_Name'])
             covid_plot = covid_plot.sort_values(['dt','Countyname','ST_Name'])
             fig = plt.figure()
-            ax2 = covid_plot.plot(column='Confirmed', cmap='PiYG_r', scheme='user_defined', classification_kwds={'bins':scheme_list})
+            ax2 = covid_plot.plot(column=f'{selected_col}', cmap='PiYG_r', scheme='user_defined', classification_kwds={'bins':scheme_list})
             ax2.set_axis_off()
             plt.savefig(opj(img_save_path,f'{uni_dt[i]}.png'), dpi=200)
             plt.clf()
@@ -41,7 +40,7 @@ def save_imgs(csv_path, map_path, img_save_path, col_name, num_bins):
             covid_plot = covid_map.merge(df[df['dt']==uni_dt[i]], right_on=['Countyname','ST_Name'], left_on=['Countyname','ST_Name'])
             covid_plot = covid_plot.sorrt_values(['dt','Countyname','ST_Name'])
             
-            ax2 = covid_plot.plot(column='Confirmed', cmap='PiYG_r', scheme='user_defined', classification_kwds={'bins':scheme_list})
+            ax2 = covid_plot.plot(column=f'{selected_col}', cmap='PiYG_r', scheme='user_defined', classification_kwds={'bins':scheme_list})
             ax2.set_axis_off()
             plt.savefig(opj(img_save_path,f'{uni_dt[i]}.png'), dpi=200)
             plt.clf()
